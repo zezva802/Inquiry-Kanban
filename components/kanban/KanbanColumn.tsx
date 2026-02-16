6741cebf406515b57d70d5f59f8631b2565c8f4b
zezva <kobaidze.zezva@kiu.edu.ge>
2026-02-16 20:33:52 +0400

    feat: add column headers

--- components/kanban/KanbanColumn.tsx
+++ components/kanban/KanbanColumn.tsx
@@ -8,9 +8,26 @@ interface KanbanColumnProps {
 }
 
 export function KanbanColumn({ title, phase, inquiries}: KanbanColumnProps){
+
+    const count = inquiries.length;
+    const totalPotentialValue = inquiries.reduce((acc, i) => {return acc + i.potentialValue}, 0);
+    const formatter = new Intl.NumberFormat('en-CH', {
+        style: 'currency',
+        currency: 'CHF',
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+    });
+    const formattedValue = formatter.format(totalPotentialValue);
+
     return(
         <div className="w-80 bg-white p-4 rounded-lg mx-2 min-h-screen">
-            {title}
+            <header className="mb-4">
+                <h2 className="font-bold text-gray-800">{title}</h2>
+                <div className="text-sm text-gray-600">
+                    <p>{count} inquiries</p>
+                    <p>Total: {formattedValue}</p>
+                </div>
+            </header>
             {inquiries.map((inquiry) => (
                 <InquiryCard key={inquiry.id} inquiry={inquiry}/>
             ))}
